2537cd8e12927b1162588bebf8f303f018fce80d
Mateus Gonçalves Geracino <mateusgoncalvesgeracino@hotmail.com>
2019-07-26 20:26:20 -0300

    Add execute query generic

--- source/Ecommerce.Repository/Repository.cs
+++ source/Ecommerce.Repository/Repository.cs
@@ -3,23 +3,50 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using Dapper;
 using Dapper.Contrib.Extensions;
 using Ecommerce.Domain.Models;
 using Ecommerce.Repository.Interfaces;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace Ecommerce.Repository
 {
     public class Repository<T> : IRepository<T> where T : Entity
     {
         private readonly IConfiguration _config;
+        private readonly ILogger<Repository<T>> _logger;
 
-        public Repository(IConfiguration config)
+        public Repository(IConfiguration config, ILogger<Repository<T>> logger)
         {
             _config = config;
+            _logger = logger;
         }
 
         private IDbConnection Conn => new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+
+        public List<T> ExecuteQuery(string query, DynamicParameters parameters = null)
+        {
+            try
+            {
+                if (parameters == null)
+                    return Conn.Query<T>(query).ToList();
+
+                return Conn.Query<T>(query, parameters).ToList();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                throw;
+            }
+            finally
+            {
+                if ((Conn != null) && (Conn.State != ConnectionState.Closed))
+                {
+                    Conn.Close();
+                }
+            }
+        }
 
         public T Insert(T obj)
         {
