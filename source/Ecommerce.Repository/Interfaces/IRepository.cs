8fd8c7f36dbf01748c47fcbe944b9c863e0c49cc
Mateus Gonçalves Geracino <mateusgoncalvesgeracino@hotmail.com>
2019-07-26 19:08:52 -0300

    Create a generic repository

---
+++ source/Ecommerce.Repository/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Ecommerce.Domain.Models;
+
+namespace Ecommerce.Repository.Interfaces
+{
+    public interface IRepository<T> : IDisposable where T : Entity
+    {
+        T Insert(T obj);
+        bool Remove(T obj);
+        List<T> GetAll();
+        T Update(T obj);
+    }
+}
---
+++ source/Ecommerce.Repository/Repository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper.Contrib.Extensions;
+using Ecommerce.Domain.Models;
+using Ecommerce.Repository.Interfaces;
+using Microsoft.Extensions.Configuration;
+
+namespace Ecommerce.Repository
+{
+    public class Repository<T> : IDisposable, IRepository<T> where T : Entity
+    {
+        private readonly IConfiguration _config;
+
+        public Repository(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        private IDbConnection Conn => new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+
+        public T Insert(T obj)
+        {
+            Conn.Insert(obj);
+            return obj;
+        }
+
+        public bool Remove(T obj)
+        {
+            return Conn.Delete(obj);
+        }
+
+        public List<T> GetAll()
+        {
+            return Conn.GetAll<T>().ToList();
+        }
+
+        public T Update(T obj)
+        {
+            Conn.Update(obj);
+            return obj;
+        }
+
+        public void Dispose() => Conn.Dispose();
+    }
+}
