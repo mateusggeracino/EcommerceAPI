8490700a4fe89e1a067947fda19c3a9913bb02f8
Mateus Gonçalves Geracino <mateusgoncalvesgeracino@hotmail.com>
2019-07-26 19:31:45 -0300

    Test push

--- source/Ecommerce.Domain/Models/Client.cs
+++ source/Ecommerce.Domain/Models/Client.cs
@@ -1,7 +1,10 @@
-﻿namespace Ecommerce.Domain.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ecommerce.Domain.Models
 {
     public class Client : Entity
     {
+        [Column("Name")]
         public string Name { get; set; }
         public string SocialNumber { get; set; }
         public string Email { get; set; }
