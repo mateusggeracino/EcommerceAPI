11af28885e56a18f61b4a58a1599926c1e50836b
Mateus Gonçalves Geracino <mateusgoncalvesgeracino@hotmail.com>
2019-07-25 22:53:17 -0300

    Initial commit

---
+++ source/Ecommerce.Domain/Models/Client.cs
@@ -0,0 +1,9 @@
+﻿namespace Ecommerce.Domain.Models
+{
+    public class Client : Entity
+    {
+        public string Name { get; set; }
+        public string SocialNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ source/Ecommerce.Domain/Models/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace Ecommerce.Domain.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+    }
+}
