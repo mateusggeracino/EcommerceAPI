5ee18d9b41405f345d309d6daa2706d278dab871
Lucas Martins <luucasmartins@hotmail.com>
2019-07-27 09:37:58 -0300

    adicionado o domain paymentmethod

---
+++ source/Ecommerce.Domain/Models/PaymentMethod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Ecommerce.Domain.Models
+{
+    [Table( "PaymentMethod" )]
+    public class PaymentMethod : Entity
+    {
+        [Column( "PMType" )]
+        public string Type { get; set; }
+        [Column( "PMSuplierId" )]
+        public int SupplierId { get; set; }
+    }
+}
