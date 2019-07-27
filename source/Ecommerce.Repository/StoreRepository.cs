ebc48409f1dabd0f98fc795e18bbb01f33951ce2
Lucas Martins <luucasmartins@hotmail.com>
2019-07-26 22:22:20 -0300

    corrigindo erros

--- source/Ecommerce.Repository/StoreRepository.cs
+++ source/Ecommerce.Repository/StoreRepository.cs
@@ -4,12 +4,13 @@ using System.Text;
 using Microsoft.Extensions.Configuration;
 using Ecommerce.Domain.Models;
 using Ecommerce.Repository.Interfaces;
+using Microsoft.Extensions.Logging;
 
 namespace Ecommerce.Repository
 {
     public class StoreRepository : Repository<Store>, IStoreRepository
     {
-        public StoreRepository( IConfiguration config ) : base( config )
+        public StoreRepository( IConfiguration config, ILogger<Repository<Store>> logger ) : base( config, logger )
         {
         }
     }
