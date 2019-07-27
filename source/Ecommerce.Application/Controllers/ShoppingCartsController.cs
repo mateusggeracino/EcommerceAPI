fa0e805ae111b23afe9faa28cd45fb13a1ebff12
Jéssica Dias Rodrigues <jeh_dias18@hotmail.com>
2019-07-27 15:27:05 -0300

    Add order

--- source/Ecommerce.Application/Controllers/ShoppingCartsController.cs
+++ source/Ecommerce.Application/Controllers/ShoppingCartsController.cs
@@ -19,38 +19,36 @@ namespace Ecommerce.Application.Controllers
             _shoppingCartServices = shoppingCartservices;
         }
 
-        // GET api/values
         [HttpGet]
         public IEnumerable<ShoppingCarts> Get()
         {
             return _shoppingCartServices.List();
         }
 
-        // GET api/values/5
         [HttpGet("{id}")]
         public ShoppingCarts Get(int id)
         {
             return _shoppingCartServices.GetById(id);
         }
 
-        // POST api/values
         [HttpPost]
         public void Post([FromBody] ShoppingCarts shoppingCarts)
         {
              _shoppingCartServices.Insert(shoppingCarts);
         }
 
-        // PUT api/values/5
         [HttpPut]
         public void Put([FromBody] ShoppingCarts shoppingCarts)
         {
             _shoppingCartServices.Update(shoppingCarts);
         }
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        [HttpPatch]
+        public void FinalizeShoppingCarts([FromBody] ShoppingCarts shoppingCarts)
         {
+
+            _shoppingCartServices.Update(shoppingCarts);
+
         }
     }
 }
