69f02b91594840640b8b8edb3d8247483fd95a20
luisgustavoguerra <luisgustavoguerra@gmail.com>
2019-07-26 22:00:14 -0300

    Commit Gustavo

--- source/Ecommerce.Application/Startup.cs
+++ source/Ecommerce.Application/Startup.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Ecommerce.Business;
+using Ecommerce.Domain.Models;
+using Ecommerce.Repository;
+using Ecommerce.Repository.Interfaces;
+using Ecommerce.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -24,7 +29,17 @@ namespace Ecommerce.Application
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            ClientDependencyInjection(services);
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        public void ClientDependencyInjection(IServiceCollection services)
+        {
+            services.AddSingleton<IRepository<Client>, Repository<Client>>();
+            services.AddTransient<ClientBusiness>();
+            services.AddTransient<Client>();
+            services.AddTransient<ClientServices>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- source/Ecommerce.Domain/Models/Client.cs
+++ source/Ecommerce.Domain/Models/Client.cs
@@ -1,9 +1,17 @@
-﻿namespace Ecommerce.Domain.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Ecommerce.Domain.Models
 {
+    [Table("customers")]
     public class Client : Entity
     {
+        public string Type { get; set; }
+        public string Document { get; set; }
         public string Name { get; set; }
-        public string SocialNumber { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+        public string Telephone { get; set; }
         public string Email { get; set; }
     }
 }
