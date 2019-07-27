a2c269d90d5a9f69f0d3e4ecc156126d6d867402
Lucas Martins <luucasmartins@hotmail.com>
2019-07-26 21:32:21 -0300

    Adicionado store

---
+++ source/Ecommerce.Repository/Interfaces/IStoreRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ecommerce.Repository.Interfaces
+{
+    public interface IStoreRepository : IRepository<Domain.Models.Store>
+    {
+    }
+}
---
+++ source/Ecommerce.Repository/StoreRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Ecommerce.Domain.Models;
+using Ecommerce.Repository.Interfaces;
+
+namespace Ecommerce.Repository
+{
+    public class StoreRepository : Repository<Store>, IStoreRepository
+    {
+        public StoreRepository( IConfiguration config ) : base( config )
+        {
+        }
+    }
+}
