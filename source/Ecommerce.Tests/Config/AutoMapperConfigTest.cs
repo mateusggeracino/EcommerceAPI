beea9ef7951cf0ffa54ea615f4746a39a9c33aa2
Lucas Martins <luucasmartins@hotmail.com>
2019-07-27 17:40:31 -0300

    criando os testes

---
+++ source/Ecommerce.Tests/Config/AutoMapperConfigTest.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Ecommerce.Application.AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ecommerce.Tests.Config
+{
+    public class AutoMapperConfigTest
+    {
+        public static IMapper GetInstance( )
+        {
+            var mapperConfig = AutoMapperConfig.RegisterMappings( );
+            return mapperConfig.CreateMapper( );
+        }
+    }
+}
