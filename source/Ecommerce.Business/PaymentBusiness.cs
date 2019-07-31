ca395e364b28fe90a76421f6f3b098d52488eae1
Mateus G. Geraicno <mateusgoncalvesgeracino@hotmail.com>
2019-07-31 19:14:33 -0300

    remove iauthorizar

--- source/Ecommerce.Business/PaymentBusiness.cs
+++ source/Ecommerce.Business/PaymentBusiness.cs
@@ -1,24 +1,16 @@
 ﻿using Ecommerce.Business.Interfaces;
 using Ecommerce.Domain.Models;
-using Ecommerce.Integration.AuthorizarApi.Interface;
-using Ecommerce.Integration.AuthorizarApi.Model;
 using Ecommerce.Repository.Interfaces;
-using RestSharp;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Ecommerce.Business
 {
     public class PaymentBusiness : IPaymentBusiness
     {
         private readonly IPaymentRepository _paymentRepository;
-        private readonly IAuthorizar _authorizar;
 
-        public PaymentBusiness(IPaymentRepository paymentRepository, IAuthorizar authorizar)
+        public PaymentBusiness(IPaymentRepository paymentRepository)
         {
             _paymentRepository = paymentRepository;
-            _authorizar = authorizar;
         }
 
         public Payment Insert(int orderId, int paymentId)
--- source/Ecommerce.Domain/Models/Order.cs
+++ source/Ecommerce.Domain/Models/Order.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Text;
 
 namespace Ecommerce.Domain.Models
 {
@@ -12,8 +10,6 @@ namespace Ecommerce.Domain.Models
         public DateTime OrderCreation { get; set; }
         public DateTime OrderExpiring { get; set; }
         public int OrderStatus { get; set; }
-
         public Decimal OrderTotalValue { get; set; }
-
     }
 }
